ad41e89dd4e4dad2b41739bf1853076c996c9e08
Turtle <hypoturtle+gh@gmail.com>
2021-04-01 12:26:06 +0100

    Switch to arcade-curtains for animation sequence.

--- aaaaAAAA/_sprites.py
+++ aaaaAAAA/_sprites.py
@@ -1,49 +1,23 @@
 import arcade
 
 DUCKY_SPEED = 240
-ducky_list = []  # Currently doesn't get populated
 
 
 class Ducky(arcade.Sprite):
     """Ducky sprite."""
 
-    def __init__(
-        self, ducky_name: str, scale: float = 1, points: list[tuple[int, int]] = None, *args, **kwargs
-    ):
+    def __init__(self, ducky_name: str, scale: float = 1, *args, **kwargs):
         self.image_file_name = f"assets/{ducky_name}.png"
-        self.path = points[:]
-        self.end = self.path.pop(0)
-        super().__init__(
-            self.image_file_name, scale, hit_box_algorithm="None", *args, **kwargs
-        )
-        # uniform swimming - conveyor belt/queue like
-        arcade.schedule(self.swim, 1)
+        super().__init__(self.image_file_name, scale, hit_box_algorithm="None", *args, **kwargs)
 
-    def update(self) -> None:
-        """Update the position of the ducky."""
-        self.center_x, self.center_y = self.end
+    @staticmethod
+    def expand(self, x: float, y: float):
+        self.width *= 1.1
+        self.height *= 1.1
+        self.draw()
 
-    def swim(self, dt: float) -> None:
-        """Have the duckies swim to their final path."""
-        self.finish = self.path.pop(0)
-        if not self.path:
-            if self in ducky_list:
-                ducky_list.remove(self)
-            arcade.unschedule(self.swim)
-        else:
-            self.progress = 0
-            self.pp = lambda _: self.partial_position(
-                self.finish, dt / DUCKY_SPEED * self.progress
-            )
-            arcade.schedule(self.pp, dt / DUCKY_SPEED)
-
-    def partial_position(self, finish: tuple[float, float], dt: float) -> None:
-        """Non-full position."""
-        end_x, end_y = finish
-        start_x, start_y = self.position
-        self.end = start_x + ((end_x - start_x) * dt), start_y + (
-            (end_y - start_y) * dt
-        )
-        self.progress += 1/DUCKY_SPEED
-        if self.progress == 1:
-            arcade.unschedule(self.pp)
+    @staticmethod
+    def shrink(self, x: float, y: float):
+        self.width /= 1.1
+        self.height /= 1.1
+        self.draw()
---
+++ aaaaAAAA/game.py
@@ -0,0 +1,96 @@
+from random import choice, randint
+from typing import Optional
+
+from aaaaAAAA import _sprites, constants
+
+import arcade
+from arcade_curtains import BaseScene, KeyFrame, Sequence, Curtains
+
+
+POINTS_HINT = [(0.0025, 0.665), (0.0575, 0.7883), (0.08375, 0.8467), (0.11875, 0.905),
+               (0.18, 0.8767), (0.245, 0.845), (0.2725, 0.89), (0.29125, 0.9133),
+               (0.29125, 0.9133), (0.33, 0.815), (0.33, 0.815), (0.3825, 0.69167),
+               (0.4, 0.67167), (0.4175, 0.73167), (0.4175, 0.73167),
+               (0.47, 0.87167), (0.5225, 0.8133), (0.5225, 0.8133),
+               (0.59, 0.71167), (0.59, 0.71167), (0.60625, 0.72167)]
+
+
+DUCKS = 36
+
+
+class DuckScene(BaseScene):
+    def setup(self):
+        self.background = arcade.load_texture("assets/main.png")
+        self.ducks = arcade.SpriteList()
+        self.leader = _sprites.Ducky(choice(constants.DUCKY_LIST), 0.075)
+        self.ducks.append(self.leader)
+        self.seq = self.sequence_gen()
+
+    def add_a_ducky(self, dt: Optional[float] = None) -> None:
+        ducky = _sprites.Ducky(choice(constants.DUCKY_LIST), 0.05)
+        self.events.hover(ducky, ducky.expand)
+        self.events.out(ducky, ducky.shrink)
+        self.ducks.append(ducky)
+        seq = self.sequence_gen(random=True)
+        self.animations.fire(ducky, seq)
+        if len(self.ducks) >= DUCKS:
+            arcade.unschedule(self.add_a_ducky)
+
+    def enter_scene(self, previous_scene) -> None:
+        self.animations.fire(self.leader, self.seq)
+        arcade.schedule(self.add_a_ducky, 3)
+
+    def draw(self):
+        """When a thing is drawn."""
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(
+            0, 0, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, self.background
+        )
+        self.ducks.draw()
+
+    @staticmethod
+    def sequence_gen(random: Optional[bool] = False) -> Sequence:
+        seq = Sequence()
+        current = 0
+        for index, ((x1, y1), (x2, y2)) in enumerate(zip(POINTS_HINT[:-1], POINTS_HINT[1:])):
+            p1 = x1 * constants.SCREEN_WIDTH, y1 * constants.SCREEN_HEIGHT
+            p2 = x2 * constants.SCREEN_WIDTH, y2 * constants.SCREEN_HEIGHT
+            frames = 1
+            if random:
+                frames = randint(1, 5)
+            seq.add_keyframes((current, KeyFrame(position=p1)), (current+frames, KeyFrame(position=p2)))
+            current += frames
+        return seq
+
+
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.curtains = Curtains(self)
+        self.curtains.add_scene('swimming_scene', DuckScene())
+        self.curtains.set_scene('swimming_scene')
+        arcade.set_background_color(arcade.color.WARM_BLACK)
+        self.path = []
+
+    def on_key_release(self, symbol: int, modifiers: int) -> None:
+        """When buttons are pressed."""
+        if symbol == 97:
+            if self.curtains.current_scene == self.curtains.scenes['swimming_scene']:
+                self.curtains.current_scene.add_a_ducky()
+        elif symbol == 112:
+            print(self.path)
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int) -> None:
+        self.path.append((x/self.window.width, y/self.window.height))
+
+
+def main():
+    window = arcade.Window()
+    arcade.set_window(window)
+    game = GameView()
+    window.show_view(game)
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
