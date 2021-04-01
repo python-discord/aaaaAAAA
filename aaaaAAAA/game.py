4210e5dfa7e5b8d7adbb9cf6b9d7e21e1ae79e50
Jannes Jonkers <jjonkers2@gmail.com>
2021-04-01 23:58:13 +0200

    Fix duck pond file name.

--- aaaaAAAA/game.py
+++ aaaaAAAA/game.py
@@ -26,7 +26,7 @@ class DuckScene(BaseScene):
     def setup(self) -> None:
         """Setup the scene assets."""
         self.background = arcade.load_texture("assets/overworld/overworld placeholder.png")
-        self.pond = arcade.load_texture("assets/overworld/ponds/png/Blue pond.png")
+        self.pond = arcade.load_texture("assets/overworld/ponds/png/Blue Pond.png")
         self.pondhouse = arcade.load_texture("assets/overworld/pondhouse.png")
         self.ducks = arcade.SpriteList()
         self.leader = _sprites.Ducky(choice(constants.DUCKY_LIST), 0.075)
